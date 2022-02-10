4e96242c9c198614c85d8dcd7688d771852abf0b
Sam Collier <sam@samcollier.tech>
2022-02-10 01:18:00 +0000

    derivedAction adapter

--- Command.h
+++ Command.h
@@ -12,3 +12,19 @@ struct Command
 	unsigned int category; // The category the command is for
 };
 
+// This function takes in a function on a child of SceneNode and converts it to a function
+// on the SceneNode base class
+template <typename GameObject, typename Function>
+std::function<void(SceneNode&, const float)> derivedAction(Function fn)
+{
+	// This lambda expression uses a capture list '[=]'
+	// means that variables referenced from the body such as fn
+	// are captured by value
+	return [=] (SceneNode& node, const float dt)
+	{
+		fn(static_cast<GameObject&>(node),dt);
+	};
+}
+// derivedAction acts as an adapter for when we build commands
+// it makes the implementation of command actions look cleaner
+// since it deals with the casting for us
